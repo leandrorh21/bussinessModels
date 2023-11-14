7c33279abe158f3cbb595bbc76d54fc438a6637d
leandro <leandrorh21@gmail.com>
2023-11-14 00:16:29 -0500

    add dist

--- dist/models/generic.class.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class GenericClass {
-    constructor(params) {
-        Object.assign(this, params);
-    }
-}
-exports.default = GenericClass;
--- dist/models/order/order.class.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const generic_class_1 = __importDefault(require("models/generic.class"));
-class Order extends generic_class_1.default {
-}
-exports.default = Order;
